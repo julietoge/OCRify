8ce594a78ad78f0230d15d9195afd02588759138
julietoge <julietogechi27@gmail.com>
2023-07-05 15:57:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Tesseract from "tesseract.js";
+import "./App.css";
 
-function App() {
+const App = () => {
+
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [ocrResult, setOCRResult] = useState("");
+
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(URL.createObjectURL(file));
+  };
+
+
+  const performOCR = async () => {
+    if (!selectedImage) {
+      alert("Please upload an image first.");
+      return;
+    }
+    try {
+      const { data } = await Tesseract.recognize(selectedImage, "eng");
+      setOCRResult(data.text);
+    } catch (error) {
+      console.error("OCR error:", error);
+    }
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header>
+        <div>
+          <h1>SwiftLexi OCR</h1>
+          <p>Get words in image!</p>
+        </div>
+
+         <a href="https://">About_Us</a>
+        
       </header>
+
+      <div className="main-section">
+
+        <div className="input-wrapper">
+          <label htmlFor="Upload">Upload Image</label>
+          <input
+            type="file"
+            id="Upload"
+            accept="image/*"
+            onChange={handleImageUpload}
+          />
+        </div>
+
+        <div className="result">
+
+          {selectedImage && (
+            <div className="selectedImage-container">
+              <img src={selectedImage} alt="Selected" />
+            </div>
+          )}
+
+          <div className="btn-ocrResult-container">
+            <button onClick={performOCR}>Perform OCR</button>
+
+            {ocrResult && (
+              <div className="ocrResult-container">
+                OCR Result:
+                <p>{ocrResult}</p>
+              </div>
+            )}
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
-}
+};
 
 export default App;
