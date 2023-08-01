5775e84b7344c35f77ddfeb831edda2f1b0f00f7
julietoge <julietogechi27@gmail.com>
2023-08-01 06:53:47 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -3,64 +3,73 @@ import Tesseract from "tesseract.js";
 import "./App.css";
 
 const App = () => {
-
-
   const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedLanguage, setSelectedLanguage] = useState('eng');
   const [ocrResult, setOCRResult] = useState("");
   const [errorMess, setErrorMess] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     setSelectedImage(URL.createObjectURL(file));
+
+  //   const reader = new FileReader();
+    
+  //   reader.onload = (e) => {
+  //     setSelectedImage(e.target.result);
+  //   };
+    
+  //   reader.readAsDataURL(file);
+  };
+
+
+
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
   };
 
 
   const performOCR = async () => {
 
-    setIsLoading(true);
-    
     if (!selectedImage) {
       setErrorMess(true);
       setIsLoading(false);
       return;
-    }
-    else{
-      setErrorMess(false);
-    }
+    } 
     
-    try {
-      
-      const { data } = await Tesseract.recognize(selectedImage, "eng");
-      setOCRResult(data.text);
-    } catch (error) {
-      console.error("OCR error:", error);
-    }
+    else if(selectedImage) {
+      setErrorMess(false);
+      setIsLoading(true);
 
-    // Simulating an asynchronous operation
+      try {
+        const { data: { text }  } = await Tesseract.recognize(selectedImage, selectedLanguage);
+        setOCRResult(text);
+      } catch (error) {
+        console.error("OCR error:", error);
+      }
+
+      // Simulating an asynchronous operation
     setTimeout(() => {
       setIsLoading(false);
-    }, 2000);
+    }, 10);
+    }
 
+    
   };
-
 
   return (
     <div className="App">
-
       <header>
         <div>
           <h1>SwiftLexi OCR</h1>
           <p>Get words in image!</p>
         </div>
 
-         {/* <a href="https://">About_Us</a> */}
-        
       </header>
-      <div className="errorMess-section">{errorMess && (<div>Please upload an image first!</div>)}</div>
+      <div className="errorMess-section">
+        {errorMess && <div>Please upload an image first!</div>}
+      </div>
       <div className="main-section">
-
         <div className="input-wrapper">
           <label htmlFor="Upload">Upload Image</label>
           <input
@@ -71,8 +80,20 @@ const App = () => {
           />
         </div>
 
-        <div className="result">
 
+        <div>
+        <label htmlFor="language-select">Select Language:</label>
+        <select id="language-select" value={selectedLanguage} onChange={handleLanguageChange}>
+          <option value="eng">English</option>
+          <option value="hausa">Hausa</option>
+          <option value="igbo">Igbo</option>
+          <option value="yor">Yoruba</option>
+        </select>
+      </div>
+
+
+
+        <div className="result">
           {selectedImage && (
             <div className="selectedImage-container">
               <img src={selectedImage} alt="Selected" />
@@ -81,25 +102,21 @@ const App = () => {
 
           <div className="btn-ocrResult-container">
             <button onClick={performOCR}>
-            {isLoading ? 'Processing...' : 'Perform OCR'}
-              </button>
+              {isLoading ? "Processing..." : "Perform OCR"}
+            </button>
 
             {ocrResult && (
               <div className="ocrResult-container">
                 OCR Result:
                 <p>{ocrResult} </p>
-              <div>
-                powered by Tesseract
-                {/* &copy; 2023 Ogechi Juliet Uhegbu */}
+                <div>
+                  powered by Tesseract
                 </div>
               </div>
             )}
           </div>
-
         </div>
-
       </div>
-
     </div>
   );
 };
