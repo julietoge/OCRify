f6f925ee94291d238ffdd8537a882557c72e8b38
Ogechi Juliet Uhegbu <102523763+julietoge@users.noreply.github.com>
2023-09-07 20:54:58 +0100

    Update landingPage.jsx

--- src/pages/Landing-Page/landingPage.jsx
+++ src/pages/Landing-Page/landingPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./LandingPage.css";
 import { Link } from "react-router-dom";
 import "./landingPage.css";
 import AuthLayout from "../../components/AuthLayout/AuthLayout";
