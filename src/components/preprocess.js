2cb1eea4366af00c31318d7d846e0f11535b06a9
julietoge <julietogechi27@gmail.com>
2023-10-14 21:40:36 +0100

    updated

--- src/components/preprocess.js
+++ src/components/preprocess.js
@@ -1,18 +1,19 @@
 const preprocessImage = async (imageData) => {
+  // Create a canvas element and obtain a 2D rendering context
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
   const img = new Image();
 
+  // Set the image source to the provided image data and wait for it to decode
   img.src = imageData;
   await img.decode();
 
-  // Image Scaling (Resize the image)
-  // To achieve a better performance of OCR, the image should have more than 300 PPI (pixel per inch).
+  // Image Scaling: Resize the image to a width of 2500 pixels while maintaining the aspect ratio
   canvas.width = 2500;
   canvas.height = (canvas.width / img.width) * img.height;
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-  // Grayscale effect(grayscale conversion)
+  // Grayscale effect: Convert the image to grayscale
   const imageDataGrey = ctx.getImageData(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < imageDataGrey.data.length; i += 4) {
     const avg =
@@ -25,17 +26,63 @@ const preprocessImage = async (imageData) => {
     imageDataGrey.data[i + 2] = avg;
   }
 
-  // Binarization using a simple threshold
+  // Binarization: Convert the grayscale image into a binary image using a simple threshold
   for (let i = 0; i < imageDataGrey.data.length; i += 4) {
     imageDataGrey.data[i] = 255 - imageDataGrey.data[i];
     imageDataGrey.data[i + 1] = 255 - imageDataGrey.data[i + 1];
     imageDataGrey.data[i + 2] = 255 - imageDataGrey.data[i + 2];
   }
 
-  //   Noise Removal(optional)
+  // Noise Removal (Median Filtering): Apply a median filter to remove noise
+  const filteredImageData = medianFilter(imageDataGrey, canvas.width, canvas.height);
 
-  ctx.putImageData(imageDataGrey, 0, 0);
+  // Put the filtered data back on the canvas
+  ctx.putImageData(filteredImageData, 0, 0);
+
+  // Return the preprocessed image as a data URL in PNG format with 80% quality
   return canvas.toDataURL("image/png", 0.8);
 };
+
+// Helper function to apply median filtering to an image
+function medianFilter(imageData, width, height) {
+  const outputImageData = new ImageData(width, height);
+  const radius = 1; // Adjust the radius based on the amount of noise you want to remove
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const r = [];
+      const g = [];
+      const b = [];
+
+      // Gather pixel values within the specified radius
+      for (let dy = -radius; dy <= radius; dy++) {
+        for (let dx = -radius; dx <= radius; dx++) {
+          const nx = x + dx;
+          const ny = y + dy;
+
+          if (nx >= 0 && ny >= 0 && nx < width && ny < height) {
+            const index = (ny * width + nx) * 4;
+            r.push(imageData.data[index]);
+            g.push(imageData.data[index + 1]);
+            b.push(imageData.data[index + 2]);
+          }
+        }
+      }
+
+      // Sort the gathered values and select the median value
+      r.sort();
+      g.sort();
+      b.sort();
+
+      const index = (y * width + x) * 4;
+      outputImageData.data[index] = r[Math.floor(r.length / 2)];
+      outputImageData.data[index + 1] = g[Math.floor(g.length / 2)];
+      outputImageData.data[index + 2] = b[Math.floor(b.length / 2)];
+      outputImageData.data[index + 3] = imageData.data[index + 3]; // Alpha channel
+    }
+  }
+
+  return outputImageData;
+}
 
 export default preprocessImage;
